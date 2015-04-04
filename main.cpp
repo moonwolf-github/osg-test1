56d70fd4b1a31d046e7175a8c671ef16570d79ab
Marcin Trendota <moonwolf@poczta.onet.pl>
2015-04-04 20:06:16 +0200

    Switch WalkingManipulator base class to StandardManipulator.
    Also tried to use CameraManipulator (with no success - too much work is
    to be done). Now it's possible to rotate camera with mouse.

--- main.cpp
+++ main.cpp
@@ -39,17 +39,230 @@
 
 #include <osgDB/ReadFile>
 
-class WalkingManipulator : public osgGA::FirstPersonManipulator
+class WalkingManipulator : public osgGA::StandardManipulator
 {
-protected:
-    /*bool performMovementLeftMouseButton( const double eventTimeDelta, const double dx, const double dy )
+public:
+    WalkingManipulator() : osgGA::StandardManipulator(),
+        state(STAND), running(false)
+        /*_thrown( false ),
+        _allowThrow( true ),
+        _mouseCenterX(0.0f), _mouseCenterY(0.0f),
+        _delta_frame_time(0.01), _last_frame_time(0.0),
+        _modelSize( 0. ),
+        _verticalAxisFixed( true ),
+        _flags( flags ),
+        _relativeFlags( 0 )*/
     {
-        // world up vector
-        CoordinateFrame coordinateFrame = getCoordinateFrame( _eye );
-        Vec3d localUp = getUpVector( coordinateFrame );
-        rotateYawPitch( _rotation, dx, dy, localUp );
+
+    }
+
+    /*void home( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us )
+    {
+        _homeEye.set(0.,-1.0,0.);
+        _homeCenter.set(0.0,0.0,0.);
+        _homeUp.set(0.0,0.0,1.0);
+        const osg::Camera *camera = us.asView() ? us.asView()->getCamera() : NULL;
+        computeHomePosition(camera, true);
+        osgGA::CameraManipulator::home(ea, us);
+        setTransformation( _homeEye, _homeCenter, _homeUp );
+        _lastX = -1;//ea.getX();
+        _lastY = -1;//ea.getY();
+    }*/
+
+    void init( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us )
+    {
+        _lastX = -1;//ea.getX();
+        _lastY = -1;//ea.getY();
+        home(ea, us);
+        //std::cout << _lastX << ", " << _lastY << std::endl;
+    }
+
+    /** Set the position of the manipulator using a 4x4 matrix.*/
+    void setByMatrix( const osg::Matrixd& matrix )
+    {
+        // set variables
+        _eye = matrix.getTrans();
+        _rotation = matrix.getRotate();
+        // fix current rotation
+        //if( getVerticalAxisFixed() )
+        //fixVerticalAxis( _eye, _rotation, true );
+    }
+
+    /** Set the position of the manipulator using a 4x4 matrix.*/
+    void setByInverseMatrix( const osg::Matrixd& matrix )
+    {
+        setByMatrix( osg::Matrixd::inverse( matrix ) );
+    }
+
+    /** Get the position of the manipulator as 4x4 matrix.*/
+    osg::Matrixd getMatrix() const
+    {
+        return osg::Matrixd::rotate( _rotation ) * osg::Matrixd::translate( _eye );
+    }
+
+    /** Get the position of the manipulator as a inverse matrix of the manipulator,
+    typically used as a model view matrix.*/
+    osg::Matrixd getInverseMatrix() const
+    {
+        return osg::Matrixd::translate( -_eye ) * osg::Matrixd::rotate( _rotation.inverse() );
+    }
+
+    // doc in parent
+    void setTransformation( const osg::Vec3d& eye, const osg::Quat& rotation )
+    {
+        // set variables
+        _eye = eye;
+        _rotation = rotation;
+        // fix current rotation
+        //if( getVerticalAxisFixed() )
+        //fixVerticalAxis( _eye, _rotation, true );
+    }
+
+    // doc in parent
+    void getTransformation( osg::Vec3d& eye, osg::Quat& rotation ) const
+    {
+        eye = _eye;
+        rotation = _rotation;
+    }
+
+    // doc in parent
+    void setTransformation( const osg::Vec3d& eye, const osg::Vec3d& center, const osg::Vec3d& up )
+    {
+        // set variables
+        osg::Matrixd m( osg::Matrixd::lookAt( eye, center, up ) );
+        _eye = eye;
+        _rotation = m.getRotate().inverse();
+
+        // fix current rotation
+        //if( getVerticalAxisFixed() )
+        //fixVerticalAxis( _eye, _rotation, true );
+    }
+
+    // doc in parent
+    void getTransformation( osg::Vec3d& eye, osg::Vec3d& center, osg::Vec3d& up ) const
+    {
+        center = _eye + _rotation * osg::Vec3d( 0.,0.,-1. );
+        eye = _eye;
+        up = _rotation * osg::Vec3d( 0.,1.,0. );
+    }
+    /*bool handle( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us )
+    {
+        switch( ea.getEventType() )
+        {
+            case osgGA::GUIEventAdapter::FRAME:
+                return handleFrame( ea, us );
+            case osgGA::GUIEventAdapter::RESIZE:
+                return handleResize( ea, us );
+            default:
+                break;
+        }
+
+        if( ea.getHandled() )
+            return false;
+
+        switch( ea.getEventType() )
+        {
+            case osgGA::GUIEventAdapter::MOVE:
+                return handleMouseMove( ea, us );
+
+            /case osgGA::GUIEventAdapter::DRAG:
+                return handleMouseDrag( ea, us );/
+
+            case osgGA::GUIEventAdapter::PUSH:
+                return handleMousePush( ea, us );
+
+            case osgGA::GUIEventAdapter::RELEASE:
+                return handleMouseRelease( ea, us );
+
+            case osgGA::GUIEventAdapter::KEYDOWN:
+                return handleKeyDown( ea, us );
+
+            case osgGA::GUIEventAdapter::KEYUP:
+                return handleKeyUp( ea, us );
+
+            /case GUIEventAdapter::SCROLL:
+                if( _flags & PROCESS_MOUSE_WHEEL )
+                    return handleMouseWheel( ea, us );
+                else
+                    return false;/
+
+            default:
+                return false;
+        }
+    }
+    bool handleResize( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us )
+    {
+        init( ea, us );
+        us.requestRedraw();
         return true;
     }*/
+protected:
+    bool handleFrame( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us )
+    {
+        switch (state)
+        {
+        case WALK_FORWARD:
+            //std::cout << ea.getModKeyMask() << std::endl;
+            if (running)
+            {
+                moveForward(80);
+            }
+            else
+            {
+                moveForward(20);
+            }
+            return true;
+        case WALK_BACKWARD:
+            if (running)
+            {
+                moveForward(-80);
+            }
+            else
+            {
+                moveForward(-20);
+            }
+            return true;
+        default:
+            return false;
+        }
+    }
+
+    /*bool performMovementLeftMouseButton( const double eventTimeDelta, const double dx, const double dy )
+    {
+        return true;
+    }*/
+
+    /*void rotateYawPitch( osg::Quat& rotation, const double yaw, const double pitch, const osg::Vec3d& localUp)
+    {
+        // rotations
+        osg::Quat rotateYaw( -yaw, rotation * osg::Vec3d( 0.,1.,0. ) );
+        osg::Vec3d cameraRight( rotation * osg::Vec3d( 1.,0.,0. ) );
+        osg::Quat rotatePitch( -pitch, cameraRight );
+        osg::Quat newRotation = rotation * rotateYaw * rotatePitch;
+        rotation = newRotation;
+    }*/
+
+    void moveForward( const double distance )
+    {
+        osg::Vec3d cameraForward;
+        double angle;
+        _rotation.getRotate(angle, cameraForward);
+        cameraForward = _rotation * osg::Vec3d(0.,1.,0.);
+        /*cameraForward.x() = 1.;
+        cameraForward.y() = 0.;
+        cameraForward.z() = 0.;*/
+        std::cout << "angle: " << angle << " vec.x: " << cameraForward.x() << " vec.y: " << cameraForward.y() << " vec.z: " << cameraForward.z() << std::endl;
+
+        osg::Quat t = osg::Quat(angle, cameraForward);
+        std::cout << "x: " << t.x() << " y: " << t.y() << " z: " << t.z() << " w: " << t.w() << std::endl;
+        /*t.x() = 0.707107;
+        t.z() = 0;
+        double mag = sqrt(t.y()*t.y() + t.w()*t.w());
+        t.y() /= mag;
+        t.w() /= mag;*/
+
+        _eye += _rotation * osg::Vec3d( 0., 0., -distance );
+    }
 
     /*void applyAnimationStep( const double currentProgress, const double prevProgress )
     {
@@ -57,44 +270,98 @@ protected:
     }*/
     bool handleMouseMove( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us )
     {
-        return false;
+        if (_lastX < 0 && _lastY < 0)
+        {
+            _lastX = ea.getX();
+            _lastY = ea.getY();
+        }
+        //return false;
+        double dx = _lastX - ea.getX();
+        double dy = _lastY - ea.getY();
+        _lastX = ea.getX();
+        _lastY = ea.getY();
+        std::cout << dx << " " << dy << std::endl;
+        //centerMousePointer(ea, us);
+        // world up vector
+        osg::CoordinateFrame coordinateFrame = getCoordinateFrame( _eye );
+        osg::Vec3d localUp = getUpVector( coordinateFrame );
+        rotateYawPitch( _rotation, -dx / 100., -dy / 100., localUp);
+        osg::Quat t(_rotation);
+        std::cout << "x: " << t.x() << " y: " << t.y() << " z: " << t.z() << " w: " << t.w() << std::endl;
+        //us.requestWarpPointer(ea.getWindowWidth() / 2., ea.getWindowHeight() / 2.);
+        return true;
     }
     bool handleMousePush( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us )
     {
         if (ea.getButton() == osgGA::GUIEventAdapter::RIGHT_MOUSE_BUTTON)
         {
-            double step = 50.;
-            if (ea.getModKeyMask() && osgGA::GUIEventAdapter::MODKEY_SHIFT == osgGA::GUIEventAdapter::MODKEY_SHIFT)
-            {
-                step = 100.;
-            }
-            moveForward(step);
-            setAcceleration(DBL_MAX);
-            setVelocity(step);
+            state = WALK_FORWARD;
             return true;
         }
         return false;
     }
+    bool handleMouseRelease( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us )
+    {
+        if (ea.getButton() == osgGA::GUIEventAdapter::RIGHT_MOUSE_BUTTON)
+        {
+            state = STAND;
+        }
+        return false;
+    }
+
     virtual bool handleKeyDown( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us )
     {
+        running = (ea.getModKeyMask() & osgGA::GUIEventAdapter::MODKEY_SHIFT) != 0;
+        //std::cout << ea.getModKeyMask() << ", " << osgGA::GUIEventAdapter::MODKEY_SHIFT << ", " << (ea.getModKeyMask() & osgGA::GUIEventAdapter::MODKEY_SHIFT) << ", " << running << std::endl;
         if (ea.getKey() == osgGA::GUIEventAdapter::KEY_Up)
         {
-            double step = 50.;
-            if (ea.getModKeyMask() && osgGA::GUIEventAdapter::MODKEY_SHIFT == osgGA::GUIEventAdapter::MODKEY_SHIFT)
-            {
-                step = 100.;
-            }
-            moveForward(step);
+            state = WALK_FORWARD;
             return true;
         }
         else if (ea.getKey() == osgGA::GUIEventAdapter::KEY_Down)
         {
-            moveForward(-50.);
+            state = WALK_BACKWARD;
             return true;
         }
-
+        else if (ea.getKey() == osgGA::GUIEventAdapter::KEY_Right)
+        {
+            state = WALK_BACKWARD;
+            return true;
+        }
         return false;
     }
+
+    virtual bool handleKeyUp( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us )
+    {
+        running = ea.getModKeyMask() & osgGA::GUIEventAdapter::MODKEY_SHIFT != 0;
+        if (ea.getKey() == osgGA::GUIEventAdapter::KEY_Up)
+        {
+            state = STAND;
+            return true;
+        }
+        else if (ea.getKey() == osgGA::GUIEventAdapter::KEY_Down)
+        {
+            state = STAND;
+            return true;
+        }
+        return false;
+    }
+private:
+    enum TState
+    {
+        WALK_FORWARD,
+        WALK_BACKWARD,
+        STRAFE_LEFT,
+        STRAFE_RIGHT,
+        STAND,
+    };
+
+    TState state;
+    bool running;
+    double _lastX;
+    double _lastY;
+    osg::Quat _rotation;
+    osg::Vec3d _eye;
 };
 
 template<class T>
@@ -193,7 +460,7 @@ int main(int argc, char** argv)
     osgViewer::Viewer viewer(arguments);
 
     // set up the camera manipulator.
-    osg::ref_ptr<WalkingManipulator> manipulator = new WalkingManipulator();
+    osg::ref_ptr<WalkingManipulator> manipulator = new WalkingManipulator;
     viewer.setCameraManipulator( manipulator.get() );
 
     // add the state manipulator
